ddd4bf5f8f1eb830d525e708a9fe09152a03ca95
Michal Tóth <mtoth@iwr.uni-heidelberg.de>
2022-08-19 13:50:11 +0200

    Revert change to partitionviewentityset.hh. Grid still uses CollectiveCommunication.

--- dune/pdelab/common/partitionviewentityset.hh
+++ dune/pdelab/common/partitionviewentityset.hh
@@ -54,7 +54,7 @@ namespace Dune {
 
       using IntersectionIterator = typename GV::Traits::IntersectionIterator;
 
-      using CollectiveCommunication = typename GV::Traits::Communication;
+      using CollectiveCommunication = typename GV::Traits::CollectiveCommunication;
 
       using size_type = std::size_t;
       using dim_type = int;
