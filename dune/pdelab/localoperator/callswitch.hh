2fe43d9e70a48d6dc15df3120f226a680175480a
Christian Engwer <christi@dune-project.org>
2024-12-12 01:47:14 +0100

    fix multiple definition error

--- dune/pdelab/localoperator/callswitch.hh
+++ dune/pdelab/localoperator/callswitch.hh
@@ -345,28 +345,28 @@ namespace Dune {
 
     namespace LocalOperatorApply {
 
-      auto patternVolume = [](const auto& lop, auto&... args)
+      inline auto patternVolume = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doPatternVolume>::
           pattern_volume(lop, args...);
       };
 
-      auto patternVolumePostSkeleton = [](const auto& lop, auto&... args)
+      inline auto patternVolumePostSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doPatternVolumePostSkeleton>::
           pattern_volume_post_skeleton(lop, args...);
       };
 
-      auto patternSkeleton = [](const auto& lop, auto&... args)
+      inline auto patternSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doPatternSkeleton>::
           pattern_skeleton(lop, args...);
       };
 
-      auto patternBoundary = [](const auto& lop, auto&... args)
+      inline auto patternBoundary = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doPatternBoundary>::
@@ -374,28 +374,28 @@ namespace Dune {
       };
 
       //////////////////////
-      auto alphaVolume = [](const auto& lop, auto&... args)
+      inline auto alphaVolume = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaVolume>::
           alpha_volume(lop, args...);
       };
 
-      auto alphaVolumePostSkeleton = [](const auto& lop, auto&... args)
+      inline auto alphaVolumePostSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaVolumePostSkeleton>::
           alpha_volume_post_skeleton(lop, args...);
       };
 
-      auto alphaSkeleton = [](const auto& lop, auto&... args)
+      inline auto alphaSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaSkeleton>::
           alpha_skeleton(lop, args...);
       };
 
-      auto alphaBoundary = [](const auto& lop, auto&... args)
+      inline auto alphaBoundary = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaBoundary>::
@@ -404,28 +404,28 @@ namespace Dune {
 
 
       //////////////////////
-      auto lambdaVolume = [](const auto& lop, auto&... args)
+      inline auto lambdaVolume = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doLambdaVolume>::
           lambda_volume(lop, args...);
       };
 
-      auto lambdaVolumePostSkeleton = [](const auto& lop, auto&... args)
+      inline auto lambdaVolumePostSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doLambdaVolumePostSkeleton>::
           lambda_volume_post_skeleton(lop, args...);
       };
 
-      auto lambdaSkeleton = [](const auto& lop, auto&... args)
+      inline auto lambdaSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doLambdaSkeleton>::
           lambda_skeleton(lop, args...);
       };
 
-      auto lambdaBoundary = [](const auto& lop, auto&... args)
+      inline auto lambdaBoundary = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doLambdaBoundary>::
@@ -434,28 +434,28 @@ namespace Dune {
 
 
       //////////////////////
-      auto jacobianVolume = [](const auto& lop, auto&... args)
+      inline auto jacobianVolume = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaVolume>::
           jacobian_volume(lop, args...);
       };
 
-      auto jacobianVolumePostSkeleton = [](const auto& lop, auto&... args)
+      inline auto jacobianVolumePostSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaVolumePostSkeleton>::
           jacobian_volume_post_skeleton(lop, args...);
       };
 
-      auto jacobianSkeleton = [](const auto& lop, auto&... args)
+      inline auto jacobianSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaSkeleton>::
           jacobian_skeleton(lop, args...);
       };
 
-      auto jacobianBoundary = [](const auto& lop, auto&... args)
+      inline auto jacobianBoundary = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaBoundary>::
@@ -464,28 +464,28 @@ namespace Dune {
 
 
       //////////////////////
-      auto jacobianApplyVolume = [](const auto& lop, auto&... args)
+      inline auto jacobianApplyVolume = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaVolume>::
           jacobian_apply_volume(lop, args...);
       };
 
-      auto jacobianApplyVolumePostSkeleton = [](const auto& lop, auto&... args)
+      inline auto jacobianApplyVolumePostSkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaVolumePostSkeleton>::
           jacobian_apply_volume_post_skeleton(lop, args...);
       };
 
-      auto jacobianApplySkeleton = [](const auto& lop, auto&... args)
+      inline auto jacobianApplySkeleton = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaSkeleton>::
           jacobian_apply_skeleton(lop, args...);
       };
 
-      auto jacobianApplyBoundary = [](const auto& lop, auto&... args)
+      inline auto jacobianApplyBoundary = [](const auto& lop, auto&... args)
       {
         using LOP = std::decay_t<decltype(lop)>;
         Impl::LocalAssemblerCallSwitchHelper<LOP,LOP::doAlphaBoundary>::
